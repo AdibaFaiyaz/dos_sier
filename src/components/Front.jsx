2d48ffb8790ed53dc835a9340cac91f33dab12bd
adibafaiyaz16@gmail.com <adibafaiyaz16@gmail.com>
2023-10-09 21:04:18 +0530

    seventh

--- src/components/Front.jsx
+++ src/components/Front.jsx
@@ -4,7 +4,7 @@ function Front() {
     return (
         <div className='front'>
             <div className="middle">
-                <div className="sparkle"><svg xmlns="http://www.w3.org/2000/svg" width="195" height="239" viewBox="0 0 195 239" fill="none">
+                <div className="sparkle"><svg id='sparkle1' xmlns="http://www.w3.org/2000/svg" width="195" height="239" viewBox="0 0 195 239" fill="none">
                     <g clip-path="url(#clip0_1009_37018)">
                         <path d="M23 45.2297C34.5655 45.719 57.2179 37.3529 55.3036 -0.0256348C56.7393 15.7934 57.4571 45.2297 90 47.4314C65.1143 47.4314 55.3036 54.3787 55.3036 90.9744C55.2238 75.7262 48.6514 45.2297 23 45.2297Z" stroke="url(#paint0_linear_1009_37018)" stroke-width="2" stroke-linejoin="round" />
                         <path d="M121 89.8238C127.905 90.1088 141.429 85.2362 140.286 63.4663C141.143 72.6796 141.571 89.8238 161 91.1061C146.143 91.1061 140.286 95.1523 140.286 116.466C140.238 107.585 136.314 89.8238 121 89.8238Z" stroke="url(#paint1_linear_1009_37018)" stroke-width="2" stroke-linejoin="round" />
@@ -35,7 +35,7 @@ function Front() {
                     <div className="basic-text">I enjoy creating designs for various sorts of websites.</div>
                 </div>
                 <div className="sparkle">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="195" height="239" viewBox="0 0 195 239" fill="none">
+                    <svg id='sparkle2' xmlns="http://www.w3.org/2000/svg" width="195" height="239" viewBox="0 0 195 239" fill="none">
                         <path d="M34 45.305C41.7679 45.6276 56.9821 40.1115 55.6964 15.4663C56.6607 25.8964 57.1429 45.305 79 46.7566C62.2857 46.7566 55.6964 51.3373 55.6964 75.4663C55.6429 65.4125 51.2286 45.305 34 45.305Z" stroke="url(#paint0_linear_1009_37021)" stroke-width="2" stroke-linejoin="round" />
                         <path d="M104 89.7002C116.774 90.2324 141.793 81.1308 139.679 40.4663C141.264 57.676 142.057 89.7002 178 92.0953C150.514 92.0953 139.679 99.6534 139.679 139.466C139.59 122.878 132.331 89.7002 104 89.7002Z" stroke="url(#paint1_linear_1009_37021)" stroke-width="2" stroke-linejoin="round" />
                         <defs>
